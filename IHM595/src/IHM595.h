23be2c683e60a58f2e0fb8c216c67a4ffea83d19
Marcelo H Moraes <marceloh220@hotmail.com>
2017-03-18 11:49:53 -0300

    Update IHM595.h
    begin becomes init
    Added the method config to change de rows and columns of display.
    Exemple:
    IHM595 display(8,9,10);
    
    void setup() {
       display.config(20,4);
    }

--- IHM595/src/IHM595.h
+++ IHM595/src/IHM595.h
@@ -19,14 +19,22 @@ class IHM595 : public Print {
 private:
 	uint8_t _background;
 	uint8_t clock, data, enable;
+	uint8_t _row, _col;
+	uint8_t _addressrow[4];
 	void send(uint8_t d);
 	void cmd(uint8_t d, uint8_t c);
+	void _mode();
 public:
 	IHM595(uint8_t clock_, uint8_t data_, uint8_t enable_);
 	virtual size_t write(uint8_t c);
 	virtual size_t write(const char *s);
 	virtual size_t write(const uint8_t *s, size_t l);
-	void begin();
+	void init();
+	inline void config(uint8_t col, uint8_t row) {
+		_col = col;
+		_row = row;
+		this->_mode();
+	}
 	void clear();
 	void home();
 	void cursor(uint8_t mode);
